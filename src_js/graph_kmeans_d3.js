ebd5f499a2681bd59be05b9544f2915ebfb3f7e4
luckylwk <lrwderksen@gmail.com>
2014-03-16 21:48:49 +0000

    KMeans: Included Voronoi Tessalation

--- src_js/graph_kmeans_d3.js
+++ src_js/graph_kmeans_d3.js
@@ -16,8 +16,8 @@ function start_kmeans( $div_id, $svg_id ){
     // Width and height
     var margin = { 'random':50 },
         padding = 30;
-	var width = 700 - padding,
-	    height = 400 - padding;
+	var width = 700,
+	    height = 350;
 
 	// Generate a dynamic, random dataset
 	var numDataPoints = 3;	// Number of dummy data points to create
@@ -25,16 +25,19 @@ function start_kmeans( $div_id, $svg_id ){
 		data.pivots.push( get_random('pivot') );
 	}
     
+    // Initialize Voronoi function object.
+    var voronoi = d3.geom.voronoi().clipExtent( [ [padding, padding], [ width - padding, height - padding ] ] );
+
 	// Create scale functions
 	var xScale = d3.scale.linear()
 	                     //.domain( [ 0, d3.max( data.pivots, function(d){ return d[0]; }) + margin.random ] )
 						 .domain( [ 0, width ] )
-						 .range([padding, width + padding]);
+						 .range([ padding, width - padding ]);
 
 	var yScale = d3.scale.linear()
 						 //.domain( [ 0, d3.max( data.pivots, function(d){ return d[1]; }) + margin.random ] )
 						 .domain( [ 0, height ] )
-						 .range([ height - padding, 0 ]);
+						 .range([ height - padding, 0 + padding ]);
 
 	// Define X-axis
 	var xAxis = d3.svg.axis().scale(xScale).orient("bottom").ticks(12);
@@ -43,7 +46,7 @@ function start_kmeans( $div_id, $svg_id ){
 	var yAxis = d3.svg.axis().scale(yScale).orient("left").ticks(8);
 
 	// Create SVG element
-	var svg = d3.select( $div_id ).append("svg").attr("id", $svg_id )
+	var svg = d3.select($div_id).append("svg").attr("class","chart").attr("id",$svg_id)
 				.attr("width", width)
 				.attr("height", height);
 
@@ -51,40 +54,39 @@ function start_kmeans( $div_id, $svg_id ){
 	svg.append("g").attr("class","circle pivot")
 	    .selectAll("circle")
         .data( data.pivots ).enter().append("circle")
-        .attr("cx", function(d){ return xScale(d[0]); })
-        .attr("cy", function(d){ return yScale(d[1]); })
+        .attr("transform", function(d) { return "translate(" + d[0] + "," + d[1] + ")"; })
         .attr("r", 5);
     
     // Create datapoints group.
     svg.append("g").attr("class","circle data");
 
-    //Create X axis
-    svg.append("g").attr("class", "xAxis axis").attr("transform", "translate(0," + (height - padding) + ")").call(xAxis);
+    // Create X axis
+    svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + (height - padding) + ")").call(xAxis);
 
-    //Create Y axis
-    svg.append("g").attr("class", "yAxis axis").attr("transform", "translate(" + padding + ",0)").call(yAxis);
+    // Create Y axis
+    svg.append("g").attr("class", "y axis").attr("transform", "translate(" + padding + ",0)").call(yAxis);
 
     function get_random( type ){
         if( type=='pivot' ){
-            tmp_x = Math.random() * (width - 2*margin.random) + margin.random;
-            tmp_y = Math.random() * (height - 2*margin.random) + margin.random;
+            tmp_x = Math.random() * (width - 2*margin.random - 2*padding) + margin.random + padding;
+            tmp_y = Math.random() * (height - 2*margin.random -2*padding) + margin.random + padding;
         }
         else if( type=='data' ){
             // Generate X-coor.
             tmp_x = xy_piv[0] + (Math.random()-0.5)*150;
             // Make sure the points fall within the graph.
-            tmp_x = Math.max( 0, Math.min( tmp_x, width ) );
+            tmp_x = Math.max( padding, Math.min( tmp_x, width - padding ) );
             // Generate Y-coor.
             tmp_y = xy_piv[1] + (Math.random()-0.5)*150;
             // Make sure the points fall within the graph.
-            tmp_y = Math.max( 0, Math.min( tmp_y, height ) );
+            tmp_y = Math.max( padding, Math.min( tmp_y, height - padding ) );
         }
         return [tmp_x, tmp_y];
     }
     
     function gen_random(){
         
-        svg.select("g.pivot").transition().duration(500).style("opacity",0.1);
+        svg.select("g.pivot").transition().duration(500).style("opacity",0.05);
         
         for( var i=0; i<numDataPoints; i++ ){
             xy_piv = data.pivots[i];
@@ -97,8 +99,7 @@ function start_kmeans( $div_id, $svg_id ){
         svg.select("g.data").selectAll("circle")
             .data( data.datapoints ).enter().append("circle")
             .attr("class", function(d,i){ return "d" + i; })
-            .attr("cx", function(d){ return xScale(d[0]); })
-            .attr("cy", function(d){ return yScale(d[1]); })
+            .attr("transform", function(d) { return "translate(" + d[0] + "," + d[1] + ")"; })
             .transition().delay( function(d,i){ return i/data.datapoints.length*250; } ).duration(500)
             .attr("r", 3);
             
@@ -116,27 +117,33 @@ function start_kmeans( $div_id, $svg_id ){
 		    .selectAll("circle")
             .data( data.kmeans ).enter().append("circle")
             .attr("class",function(d,i){ return "centroid km" + i; })
-            .attr("cx", function(d){ return xScale(d[0]); })
-            .attr("cy", function(d){ return yScale(d[1]); })
+            .attr("transform", function(d) { return "translate(" + d[0] + "," + d[1] + ")"; })
             .transition().delay( function(d,i){ return i/data.kmeans.length*250; } ).duration(500)
             .attr("r", 5);
+            
+        svg.append("g").attr("class","kmeansvoronoi")
+            .selectAll("path")
+            .data( voronoi(data.kmeans) )
+            .enter().append("path")
+            .attr("class", function(d,i){ return "km" + i + " vor" })
+            .attr("d", function(d){ return "M" + d.join("L") + "Z"; } );
 		
 		
-		for( var r=0; r<6; r++ ){
+		for( var r=0; r<4; r++ ){
 		    // Run KMEANS using the specified index.
-			run_kmeans( r );
+			run_kmeans_iter( r );
 		}
 		
     }
     
-    function run_kmeans( $idx ){
+    function run_kmeans_iter( $idx ){
         
         // Fade out kmeans-init.
         if( $idx==0 ){
-            svg.select("g.kmeansinit").transition().duration(500).style("opacity",0.1);
+            svg.select("g.kmeansinit").transition().duration(500).style("opacity",0.2);
         }
         else {
-            svg.select("g.kmeans" + $idx ).transition().duration(500).style("opacity",0.1);
+            svg.select("g.kmeans" + $idx ).transition().duration(500).style("opacity",0.2);
         }
         
         // check each datapoint. assign a kmeans-cluster to it.
@@ -185,8 +192,7 @@ function start_kmeans( $div_id, $svg_id ){
 		    .selectAll("circle")
             .data( data.kmeans ).enter().append("circle")
             .attr("class",function(d,i){ return "centroid km" + i; })
-            .attr("cx", function(d){ return xScale(d[0]); })
-            .attr("cy", function(d){ return yScale(d[1]); })
+            .attr("transform", function(d) { return "translate(" + d[0] + "," + d[1] + ")"; })
             .transition().delay( function(d,i){ return i/data.kmeans.length*250; } ).duration(500)
             .attr("r", 5);
         
@@ -194,11 +200,18 @@ function start_kmeans( $div_id, $svg_id ){
         svg.select("g.kmeans" + ($idx+1) )
             .selectAll("line")
             .data( data.kmeans ).enter().append("line")
-            .attr("x1", function(d){ return xScale(d[2]); } )
-            .attr("y1", function(d){ return yScale(d[3]); } )
-            .attr("x2", function(d){ return xScale(d[0]); } )
-            .attr("y2", function(d){ return yScale(d[1]); } )
-            .attr("class", function(d,i){ return "km" + i; } );
+            .attr("x1", function(d){ return 0; } )
+            .attr("y1", function(d){ return 0; } )
+            .attr("x2", function(d){ return d[2]-d[0]; } )
+            .attr("y2", function(d){ return d[3]-d[1]; } )
+            .attr("class", function(d,i){ return "km" + i; } )
+            .attr("transform", function(d) { return "translate(" + d[0] + "," + d[1] + ")"; });
+            
+        svg.select(".kmeansvoronoi")
+            .selectAll("path")
+            .data( voronoi(data.kmeans) )
+            .transition().duration(500)
+            .attr("d", function(d){ return "M" + d.join("L") + "Z"; } );
         
     }
     
